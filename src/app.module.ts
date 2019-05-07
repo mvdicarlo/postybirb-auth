439d8cb025c5dcfbd0258a75de97ebf41e80eb45
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-05-07 12:12:49 +0000

    Infinite retries to db connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,11 @@ import { TumblrModule } from './tumblr/tumblr.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://postybirb:${process.env.DB_PASSWORD}@ds247648.mlab.com:47648/postybirb`, { useNewUrlParser: true }),
+    MongooseModule.forRoot(`mongodb://postybirb:${process.env.DB_PASSWORD}@ds247648.mlab.com:47648/postybirb`, {
+      useNewUrlParser: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000
+    }),
     MastodonModule,
     TwitterModule,
     DeviantArtModule,
